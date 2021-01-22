1e4f1a43e868a55b9fad302388d1fd1b8052fb56
shafin22115 <shafin22115@gmail.com>
2021-01-22 19:52:12 +0600

    finish

---
+++ assainment-3.js
@@ -0,0 +1,58 @@
+
+
+// kilometar To Meter
+function kilometerToMeter(kilometar){
+    var converter = kilometar * 1000;
+    return converter
+}
+var kilometar = kilometerToMeter(5)
+console.log(kilometar)
+
+
+// budgetCalculator
+function budgetCalculator(watch, phone, laptop){
+    var totalPrice = (watch*50) + (phone*100) + (laptop*500);
+    return totalPrice;
+}
+var totalprice = budgetCalculator(2,3,5);
+console.log(totalprice);
+
+// hotelcost
+function hotelCost(number){
+    var day = 0;
+    if(number <= 10){
+        day = 10 * 100;
+    }
+    else if(number <= 20) {
+        var firstPart = 10 * 100;
+        var remainingDay = number - 10;
+        var secondPart = remainingDay * 80;
+         day = firstPart + secondPart;
+    }
+    else{
+        var firstPart = 10 * 100;
+        var secondPart = 10 * 80;
+        var remainingDay = number - 20;
+        var thirdpart = remainingDay * 50;
+        day = firstPart + secondPart + thirdpart;
+    }
+    return day
+}
+var totalprice = hotelCost(30);
+console.log(totalprice);
+
+// mega friend
+function megaFriend(friendName){
+var largestNameLength = 0;
+var largestNameIndexInArray;
+for(var i = 0; i < friendName.length; i++){
+    if(friendName[i].length > largestNameLength){
+        largestNameLength = friendName[i].length;
+        largestNameIndexInArray = i;
+    }
+}
+return friendName[largestNameIndexInArray];
+}
+
+var FriendsNameArry = ["tamim iqbal","sha fin", "neimar", "aminul rahat"]
+console.log(megaFriend(FriendsNameArry))
