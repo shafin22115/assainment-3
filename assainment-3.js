9d5dd1c01d53eb5e46fe70d6a88c965ec39b68b1
shafin22115 <shafin22115@gmail.com>
2021-01-22 20:15:27 +0600

    finish

--- assainment-3.js
+++ assainment-3.js
@@ -1,11 +1,11 @@
-
+// https://github.com/shafin22115/assainment-3
 
 // kilometar To Meter
 function kilometerToMeter(kilometar){
     var converter = kilometar * 1000;
     return converter
 }
-var kilometar = kilometerToMeter(5)
+var kilometar = kilometerToMeter(6)
 console.log(kilometar)
 
 
@@ -14,7 +14,7 @@ function budgetCalculator(watch, phone, laptop){
     var totalPrice = (watch*50) + (phone*100) + (laptop*500);
     return totalPrice;
 }
-var totalprice = budgetCalculator(2,3,5);
+var totalprice = budgetCalculator(2,4,5);
 console.log(totalprice);
 
 // hotelcost
@@ -53,6 +53,5 @@ for(var i = 0; i < friendName.length; i++){
 }
 return friendName[largestNameIndexInArray];
 }
-
-var FriendsNameArry = ["tamim iqbal","sha fin", "neimar", "aminul rahat"]
+var FriendsNameArry = ["tamim iqbal","sha fin", "neimar", "aminul rahat", "saimon"]
 console.log(megaFriend(FriendsNameArry))
